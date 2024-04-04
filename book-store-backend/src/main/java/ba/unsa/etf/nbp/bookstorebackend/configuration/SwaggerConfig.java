7c7de2b20f737e7423367dc3ed6e8a6bdb2eea3c
Senija <skaleta1@etf.unsa.ba>
2024-04-04 12:23:58 +0200

    swagger added

---
+++ book-store-backend/src/main/java/ba/unsa/etf/nbp/bookstorebackend/configuration/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package ba.unsa.etf.nbp.bookstorebackend.configuration;
+
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public-apis")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+}
